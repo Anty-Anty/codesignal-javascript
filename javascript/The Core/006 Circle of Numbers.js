cc996547396b0bd3f51d530477e0bc1c8a26039d
Anty-Anty <79324714+Anty-Anty@users.noreply.github.com>
2023-04-12 19:24:40 -0700

    The Core/006 Circle of Numbers.js

---
+++ javascript/The Core/006 Circle of Numbers.js
@@ -0,0 +1,11 @@
+//https://app.codesignal.com/arcade/code-arcade/intro-gates/vExYvcGnFsEYSt8nQ
+
+function solution(n, firstNumber) {
+    if (firstNumber < n/2) {
+        return firstNumber + n/2
+    } 
+    else if (firstNumber >= n/2) {
+        return firstNumber - n/2
+    }
+    
+}
