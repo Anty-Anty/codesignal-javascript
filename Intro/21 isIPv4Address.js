a554f6781d8ae7bdcd277e255fb681715736134c
Anty-Anty <79324714+Anty-Anty@users.noreply.github.com>
2023-05-29 21:48:18 -0700

    add Intro/21 isIPv4Address.js

---
+++ Intro/21 isIPv4Address.js
@@ -0,0 +1,6 @@
+//https://app.codesignal.com/arcade/intro/level-5/veW5xJednTy4qcjso
+
+function solution(inputString) {
+    const result = /^([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$/.test(inputString);
+    return result
+}
