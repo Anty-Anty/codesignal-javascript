f106806e00d4a374f86ada52a50b9bbd8bd691f2
Anty-Anty <79324714+Anty-Anty@users.noreply.github.com>
2024-08-16 21:15:30 -0700

    The Core/011 Extra Number.js

---
+++ The Core/011 Extra Number.js
@@ -0,0 +1,7 @@
+//https://app.codesignal.com/arcade/code-arcade/at-the-crossroads/sgDWKCFQHHi5D3Szj
+
+function solution(a, b, c) {
+    if (a == b) {return c}
+    else if (b == c) {return a}
+    else if (a == c) {return b}
+}
